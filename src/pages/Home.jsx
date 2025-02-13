c8824b2b938ef390ee1833e27bc686542458ab1d
Mithil1324 <mithileshsp.cs24@bitsathy.ac.in>
2025-02-13 15:14:32 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,42 @@
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import useStore from "../store/store";
 
-export default function Home(){
-    const {username , password , setUsername , setPassword} = useStore();
-    const navigate = useNavigate(); 
-    const handlelogin=(e)=>{
-        e.preventDefault();
-        if(username==="admin"&&password==="admin"){
-            navigate("/Data");
-        }
-        else{
-            alert("Please enter password and username");
-        }
+export default function Home() {
+  const { username, password, setUsername, setPassword } = useStore();
+  const navigate = useNavigate();
+  const handlelogin = (e) => {
+    e.preventDefault();
+    if (username === "admin" && password === "admin") {
+      navigate("/Data");
+    } else {
+      alert("Please enter password and username");
     }
-    return ( 
-        <form onSubmit={handlelogin}>
-        <><div>
-            <input
-                id="nm"
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)} />
-        </div><div>
-                <input
-                    id="pw"
-                    type="password"
-                    placeholder="enter pass"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <button id="Login" type="submit">Login</button></>
-        </form>
-    )
+  };
+  return (
+    <form onSubmit={handlelogin}>
+      <>
+        <div>
+          <input
+            id="nm"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            id="pw"
+            type="password"
+            placeholder="enter pass"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button id="Login" type="submit">
+          Login
+        </button>
+      </>
+    </form>
+  );
 }
